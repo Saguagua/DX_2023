3b2ee1951a14435485b4e945183fbe113d6e7c3f
Saguagua <93913569+Saguagua@users.noreply.github.com>
2023-05-24 19:49:54 +0900

    셰이더 수정

--- MYDX/MYDX/Shader/TextureShaderPS.hlsl
+++ MYDX/MYDX/Shader/TextureShaderPS.hlsl
@@ -1,14 +1,14 @@
-Texture2D resource : register(t0); //GPU의 레지스터 -> 저장공간, t0번째 슬롯에 저장
+Texture2D resource : register(t0); //GPU의 레지스터 -> 저장공간, t0번째 슬롯에 저장 (정보 전달)
 SamplerState samp : register(s0);
 
 struct PixelInput
 {
 	float4 pos : SV_POSITION;
 	float4 color : COLOR;
-	float uv : UV;
+	float2 uv : UV;
 }
 
-auto PS(PixelInput input) : SV_TARGET
+float4 PS(PixelInput input) : SV_TARGET
 {
-	return resource.Sample(samp. inp);
+	return resource.Sample(samp, input.uv);
 }
